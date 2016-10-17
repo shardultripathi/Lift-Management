c497e2015d0be2121b0c8bb69b721d000f957a68
pan1997 <pankajkumaryadav1997@gmail.com>
2016-10-18 02:48:04 +0530

    lifts, liftworld,actions defined

--- lift_world.h
+++ lift_world.h
@@ -6,11 +6,25 @@
 #define COL333_A4_LIFT_WORLD_H
 
 namespace COL333_A4 {
+    /*
+     * AS still
+     * AU up
+     * AD down
+     * AOU open with up display
+     * AOD open with down display
+     */
+    enum lift_action{AS=0,AU=1,AD=2,AOU=4,AOD=5};
+
+
     class lift {
         int current_floor;
         int *drop_count;
     public:
         lift(int n) : current_floor(0), drop_count(new int[n]) { }
+
+        void update_floor(int delta) {
+            current_floor += delta;
+        }
     };
 
     class lift_world {
